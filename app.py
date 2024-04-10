bb80d6aadf311572d25a065dba02af7d60c03969
JohnPaul Mbaka <mbakajohnpaul@gmail.com>
2024-04-10 09:24:36 +0100

    running the code directly

--- app.py
+++ app.py
@@ -8,18 +8,32 @@ from fastapi import FastAPI
 from Darb import Darb
 import numpy as np
 import pickle
+# starteted modifying
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 app = FastAPI()
-# model = joblib.load("random_mod.joblib")
-model_file_path = "random_model-1.pkl"
-# pickle_in = open("random_model-1.pkl","rb")
-# classifier = pickle.load(pickle_in)
-try:
-    with open(model_file_path, "rb") as pickle_file:
-        classifier = pickle.load(pickle_file,dtype='float64')
-except Exception as e:
-    print("Error loading pickled object:", e)
+# # model = joblib.load("random_mod.joblib")
+# model_file_path = "random_model-1.pkl"
+# # pickle_in = open("random_model-1.pkl","rb")
+# # classifier = pickle.load(pickle_in)
+# try:
+#     with open(model_file_path, "rb") as pickle_file:
+#         classifier = pickle.load(pickle_file,dtype='float64')
+# except Exception as e:
+#     print("Error loading pickled object:", e)
+
+# Started modifying
+df = pd.read_csv('diabete.csv')
+X = df.drop('class',axis = 1)
+y = df['class']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3,random_state=0)
+from sklearn.ensemble import RandomForestClassifier
+rfc = RandomForestClassifier(n_estimators = 100)
+rfc.fit(X_train,y_train)
+# Set the check_input parameter to False to suppress the warning during prediction
+rfc.check_input = False
+
 
 @app.get('/')
 def index():
@@ -43,7 +57,7 @@ def predict_diabetes(data:Darb):
     pedi = data['pedi']
     age = data['age']
 
-    prediction = classifier.predict([[preg,plas,pres,skin,insu,mass,pedi,age]])
+    prediction = rfc.predict([[preg,plas,pres,skin,insu,mass,pedi,age]])
     return{
         'prediction': str(prediction)
     }
